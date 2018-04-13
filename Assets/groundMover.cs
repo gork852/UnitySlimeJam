78e6245f496e125ce3c9d310b56e21386e4ad998
AndrewM <armcmuldroch@gmail.com>
2018-04-12 22:31:26 -0700

    treadmill ground! and some ground art!

---
+++ Assets/groundMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundMover : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 startPos;
+	void Start () {
+        startPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.x + 50 < GameDirector.Director.Player.transform.position.x)
+        {
+            this.transform.position = this.transform.position + new Vector3(200, 0, 0);
+        }
+	}
+}
